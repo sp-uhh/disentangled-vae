c877948c50d0ca2e70f2801ad01c7e5d040b9bf6
carbajal <carbajal@informatik.uni-hamburg.de>
2021-05-03 16:15:06 +0200

    add clean audio in test set + corrected upsampled suffix

--- ntcd_scripts/create_audio_set_ntcd_noise.py
+++ ntcd_scripts/create_audio_set_ntcd_noise.py
@@ -7,6 +7,7 @@ import os
 import concurrent.futures # for multiprocessing
 import time
 import pickle
+from tqdm import tqdm
 from shutil import copyfile
 
 # Parameters
@@ -17,14 +18,14 @@ noise_dataset_name = 'ntcd_timit'
 if speech_dataset_name == 'ntcd_timit':
     from packages.dataset.ntcd_timit import video_list, speech_list
 if noise_dataset_name == 'ntcd_timit':
-    from packages.dataset.ntcd_timit import noisy_clean_pair_dict, noisy_speech_dict
+    from packages.dataset.ntcd_timit import noisy_clean_pair_dict, noisy_speech_dict, speech_list
 
 # Parameters
 ## Dataset
 dataset_types = ['test']
 
-# dataset_size = 'subset'
-dataset_size = 'complete'
+dataset_size = 'subset'
+# dataset_size = 'complete'
 
 # Labels
 labels = 'vad_labels'
@@ -56,10 +57,17 @@ ibm_threshold = 50 # Hard threshold
 input_video_dir = os.path.join('data', dataset_size, 'raw/')
 output_wav_dir = os.path.join('data', dataset_size, 'processed/')
 
+def process_write_clean_audio(args):
+    # Separate args
+    input_clean_file_path, output_clean_file_path = args[0], args[1]
+
+    output_wav_file = output_wav_dir + output_clean_file_path
+    os.makedirs(os.path.dirname(output_wav_file), exist_ok=True)
+
+    copyfile(input_video_dir + input_clean_file_path, output_wav_file)
 
 def process_write_noisy_audio(args):
     # Separate args
-    #TODO: modify
     noisy_file_path, clean_file_path = args[0], args[1]
 
     # Copy noisy files to processed
@@ -77,6 +85,29 @@ def main():
 
     for dataset_type in dataset_types:
 
+        # Clean speech
+        input_clean_file_paths, \
+            output_clean_file_paths = speech_list(input_speech_dir=input_video_dir,
+                                dataset_type=dataset_type)
+
+        args = [[input_clean_file_path, output_clean_file_path]
+                        for input_clean_file_path, output_clean_file_path\
+                            in zip(input_clean_file_paths, output_clean_file_paths)]
+
+        t1 = time.perf_counter()
+        
+        # Process targets
+        for i, arg in tqdm(enumerate(args)):
+            process_write_clean_audio(arg)
+
+        # with concurrent.futures.ProcessPoolExecutor(max_workers=None) as executor:
+        #     # train_stats = executor.map(process_write_label, args)
+        #     executor.map(process_write_clean_audio, args)
+
+        t2 = time.perf_counter()
+        print(f'Finished in {t2 - t1} seconds')
+
+
         # Dict mapping noisy speech to clean speech
         noisy_clean_pair_paths = noisy_clean_pair_dict(input_speech_dir=input_video_dir,
                                             dataset_type=dataset_type,
@@ -87,6 +118,7 @@ def main():
                                             dataset_type=dataset_type,
                                             dataset_size=dataset_size)
 
+
         # loop over inputs for the statistics
         args = list(noisy_clean_pair_paths.items())
         
@@ -94,11 +126,11 @@ def main():
 
             t1 = time.perf_counter()
 
-            # for i, arg in tqdm(enumerate(args)):
-            #     process_write_noisy_audio(arg)
+            for i, arg in tqdm(enumerate(args)):
+                process_write_noisy_audio(arg)
 
-            with concurrent.futures.ProcessPoolExecutor(max_workers=None) as executor:
-                executor.map(process_write_noisy_audio, args)
+            # with concurrent.futures.ProcessPoolExecutor(max_workers=None) as executor:
+            #     executor.map(process_write_noisy_audio, args)
 
             t2 = time.perf_counter()
             print(f'Finished in {t2 - t1} seconds')
